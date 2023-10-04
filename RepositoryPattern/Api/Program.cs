68d35cc1b605cea3a3e49f6d297377054449ba84
PHI HOANG CAO <hcphi@misa-group.vn>
2023-10-04 11:30:48 +0700

    finish source code Repo pattern

--- RepositoryPattern/Api/Program.cs
+++ RepositoryPattern/Api/Program.cs
@@ -1,6 +1,63 @@
+using DataAccess.EFCore;
+using DataAccess.EFCore.Repositories;
+using DataAccess.EFCore.UnitOfWork;
+using Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// connect to mysql with connection string from app settings
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+if(connectionString != null)
+{
+    builder.Services.AddDbContext<ApplicationContext>(options =>
+     options.UseMySQL(connectionString));
+}
+
+
+builder.Services.AddTransient(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+builder.Services.AddTransient<IDeveloperRepository, DeveloperRepository>();
+builder.Services.AddTransient<IProjectRepository, ProjectRepository>();
+builder.Services.AddTransient<IUnitOfWork, UnitOfWork>();
+
+
+builder.Services.AddControllers();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// CreateDbIfNotExists
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    try
+    {
+        var context = services.GetRequiredService<ApplicationContext>();
+        context.Database.EnsureCreated();
+    }
+    catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "An error occurred creating the DB.");
+    }
+}
+
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
 
 app.Run();
